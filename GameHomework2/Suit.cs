a8364705158d9b7109a1ae6243a0f00cd8f85ef0
Austin <hifto15@gmail.com>
2016-09-20 16:01:34 -0400

    Created Suit class with empty methods

---
+++ GameHomework2/Suit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHomework2
+{
+    class Suit
+    {
+        static string CLUBS = "club_symbol";
+        static string DIAMONDS = "diamond_symbol";
+        static string HEARTS = "heart_symbol";
+        static string SPADES = "spade_symbol";
+        static List<Suit> VALUES;
+
+        /// <summary>
+        /// Returns 0 if the ranks are the same, -1 if this object is
+        /// lower than the other, 1 if this object is higher than the
+        /// other
+        /// </summary>
+        /// <param name="OtherSuitObject"></param>
+        /// <returns></returns>
+        int CompareTo(Suit OtherSuitObject)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Symbol associated with the rank
+        /// </summary>
+        /// <returns></returns>
+        string GetSymbol()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Name asscociated with the rank
+        /// </summary>
+        /// <returns></returns>
+        string GetName()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Override tostring method
+        /// </summary>
+        /// <returns></returns>
+        string ToString()
+        {
+            return "";
+        }
+    }
+}
