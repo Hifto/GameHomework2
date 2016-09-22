3a0adb4e502506357fa0b9df742787575b1208f4
Austin <hifto15@gmail.com>
2016-09-22 15:04:22 -0400

    CardCountHand class created along with placeholder methods

---
+++ GameHomework2/CardCountHand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHomework2
+{
+    class CardCountHand : Hand
+    {
+        //PLACEHOLDER METHODS START
+        public override int CompareTo(Hand OtherHandObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int EvaluateHand()
+        {
+            throw new NotImplementedException();
+        }
+        //PLACEHOLDER METHODS END
+    }
+}
