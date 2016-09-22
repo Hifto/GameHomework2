0ad914ba720a1ea6597c803706224eab65c9924c
Austin <hifto15@gmail.com>
2016-09-22 15:08:38 -0400

    Created BlackJackGame and CardCountGame classes

---
+++ GameHomework2/CardCountGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHomework2
+{
+    class CardCountGame
+    {
+    }
+}
