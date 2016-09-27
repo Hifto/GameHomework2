0a582080d9c403e9ea801d072f9613ce7ff658ee
hifto <hifto15@gmail.com>
2016-09-27 01:46:38 -0400

    Wrote more of the BlackJackGame class

--- GameHomework2/BlackJackGame.cs
+++ GameHomework2/BlackJackGame.cs
@@ -8,6 +8,8 @@ namespace GameHomework2
 {
     class BlackJackGame
     {
+        private static readonly Random rng = new Random();
+
         public static string PlayGame()
         {
             BlackJackHand userHand = new BlackJackHand();
@@ -15,7 +17,7 @@ namespace GameHomework2
 
             Deck deck = new Deck();
 
-            //Creates a six sets of a standard 52 card deck
+            //Creates six sets of a standard 52 card deck
             for (int n = 0; n < 6; n++)
             {
                 for (int o = 0; o < Suit.VALUES.Count; o++)
@@ -28,7 +30,33 @@ namespace GameHomework2
                 }
             }
 
+            //Welcome message
+            Console.WriteLine("Welcome to BlackJack!\n");
 
+            //Adds starting cards to hand
+            userHand.AddCard(deck.DealOne());
+            Console.WriteLine("You have been dealt the: " + userHand.GetCardAtIndex(0).GetRank().GetName() + userHand.GetCardAtIndex(0).GetSuit().GetName() + ".\n");
+            userHand.AddCard(deck.DealOne());
+            Console.WriteLine("You have been dealt the: " + userHand.GetCardAtIndex(1).GetRank().GetName() + userHand.GetCardAtIndex(1).GetSuit().GetName() + ".\n");
+
+            Console.WriteLine("Hit or Stand (H/S)?\n");
+
+            string answer = Console.ReadLine().ToUpper();
+
+            if (answer.Equals("H"))
+            {
+                int indexValue = 0;
+
+                for (int n = 0; n < deck.deckList.Count; n++)
+                {
+                    userHand.AddCard(deck.DealOne());
+                    Console.WriteLine("You have been dealt the: " + userHand.GetCardAtIndex(indexValue).GetRank().GetName() + userHand.GetCardAtIndex(indexValue).GetSuit().GetName() + ".\n");
+                    indexValue++;
+
+                    Console.WriteLine("Hit or Stand (H/S)?\n");
+                }
+
+            }
         }
     }
 }
--- GameHomework2/CardCountHand.cs
+++ GameHomework2/CardCountHand.cs
@@ -100,9 +100,6 @@ namespace GameHomework2
 
                 count++;
             }
-
-            //Console.WriteLine("The total for this hand is: " + total);
-
             return total;
         }
     }
